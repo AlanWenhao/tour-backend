dc8170b51e4764d04b6bae8d7de0faf04e0cf991
zhangwenhao <yuerang.zwh@raycloud.com>
2018-11-18 16:59:25 +0800

    add config.sample.js

---
+++ config.sample.js
@@ -0,0 +1,11 @@
+module.exports = {
+    database: {
+        DATABASE: 'your database name',
+        USERNAME: 'you sql username',
+        PASSWORD: 'password',
+        HOST: 'localhost'
+    },
+    HOST: 'localhost',
+    PORT: 4000,
+    SECRET_KEY: 'jwt scret key'
+}
