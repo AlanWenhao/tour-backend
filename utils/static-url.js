8fbd5211d2ab9e318c83a6745abbf0a4722d152d
AlanWenhao <13454782783@163.com>
2018-12-18 00:03:25 +0800

    add static img url

---
+++ utils/static-url.js
@@ -0,0 +1,6 @@
+module.exports = [
+    'https://user-gold-cdn.xitu.io/collection/default-images/2.jpg?imageView2/1/w/800/h/400/q/85/format/jpg/interlace/1',
+    'https://user-gold-cdn.xitu.io/collection/default-images/4.jpg?imageView2/1/w/800/h/400/q/85/format/jpg/interlace/1',
+    'https://user-gold-cdn.xitu.io/collection/default-images/3.jpg?imageView2/1/w/800/h/400/q/85/format/jpg/interlace/1',
+    
+]
